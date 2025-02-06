35aac75bfb269d2e61a4f46fb08eda8359f03132
mahmoudmohamed17 <ma5975986@gmail.com>
2025-02-06 09:13:04 +0200

    changes

--- lib/core/funcs/generate_random_number.dart
+++ lib/core/funcs/generate_random_number.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 int generateRandomNumber() {
   final random = Random();
+  //generate random numbers starting from 1750 to 6550
   int number = random.nextInt(4800) + 1750;
   return number;
 }
